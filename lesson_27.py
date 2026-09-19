2a0c658185c23666152c4086f8d689317bb0fafa
VladimirMonin <moninvladimir88@gmail.com>
2026-09-19 14:19:47 +0500

    lesson_27: посмотрели на специальный метод __str__ чтобы сделать принт объекта красивым. Посмотрели на методы экземпляра, поговорили об участии self, в них.  Написали методы add_product и is_product и проверили их работу в т.ч. на дебаггере

--- lesson_27.py
+++ lesson_27.py
@@ -8,23 +8,35 @@ class Fridge:
     model = "Бирюса 26 Pro Max"
     year = 2026
 
-    def __init__(self, color: str, serial_number: int):
+    def __init__(self, color: str, serial_number: str):
         self.color = color
         self.serial_number = serial_number
         self.products: list = []
 
+    def __str__(self):
+        result_str = f'Модель: "{self.model}\nГод выпуска:{self.year}\nСерийный номер: {self.serial_number}\nКол-во продуктов: {len(self.products)}"'
+        return result_str
 
-my_fridge = Fridge("чёрный", 1)
-my_father_fridge = Fridge("белый", 2)
+    def add_product(self, new_product: str) -> None:
+        if isinstance(new_product, str):
+            self.products.append(new_product)
+        else:
+            raise ValueError("В холодильник можно ложить только строки!")
 
-my_fridge.products.append("колбаска")
-my_father_fridge.products.append("коньяк")
+    def is_product(self,  check_product: str) -> bool:
+        return check_product.lower() in [product.lower() for product in self.products]
 
-print(my_fridge.products)
-print(my_fridge.color)
-print(my_fridge.model)
 
-print(my_father_fridge.products)
-print(my_father_fridge.color)
-print(my_father_fridge.model)
 
+my_fridge = Fridge("чёрный", "0001")
+my_father_fridge = Fridge("белый", "0002")
+
+
+my_fridge.products.append("Молоко") # Так мы больше не делаем
+my_fridge.add_product("Айран")
+
+print(my_fridge.is_product("АЙРАН"))
+print(my_fridge.is_product("МоЛоКО"))
+
+print(my_fridge)
+print(my_father_fridge)
