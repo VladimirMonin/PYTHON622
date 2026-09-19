22fad42dc5b378d79d64522aa9334ecb4bb614ef
VladimirMonin <moninvladimir88@gmail.com>
2026-09-19 12:27:50 +0500

    lesson_27: знакомство с ООП первый класс Fridge (пустой)

---
+++ lesson_27.py
@@ -0,0 +1,12 @@
+"""
+Lesson 27 - Знакомство с ООП
+"""
+
+class Fridge:
+    ...
+
+my_fridge = Fridge()
+print(type(my_fridge)) # <class '__main__.Fridge'>
+print(id(my_fridge)) # 1725917400432
+print(my_fridge) # <__main__.Fridge object at 0x00000191D8B18D70>
+
