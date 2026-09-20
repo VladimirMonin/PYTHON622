26f23e87906f8f88b5da66daf84339fa670ed549
VladimirMonin <moninvladimir88@gmail.com>
2026-09-20 14:05:32 +0500

    lesson_28: Посмотрели на логику работы методов класса на примере обновления версии прошивки на сервере через обновление атрибута класса `server_soft_version`, а также на то, как мы можем обновить софт конкретного экземпляра холодильника через метод экземпляра `update_soft`.
    Поговорили о том, как получается, что мы можем дотянуться до атрибута класса из `self`, из метода экземпляра. Python, не находя конкретного атрибута в самом экземпляре, идет искать его среди атрибутов класса.
    
    Этот механизм реализован в методе экземпляра `UpdateSoft`.

--- lesson_28.py
+++ lesson_28.py
@@ -5,14 +5,14 @@ Lesson 28
 """
 
 class Fridge:
-    soft_version: str = "0.0.1"
+    server_soft_version: str = "0.0.1"
     server_url: str = "www.birusa.ru/api/v1/"
     avalible_models: list[str] = ["Бирюса", "Бирюса Плюс", "Бирюса Про", "Бирюса Про Макс"]
 
     def __init__(self, owner:str, model:str):
         self.owner = owner
         self. model = self.model_validator(model)
-        self.current_soft_version = self.soft_version
+        self.current_soft_version = self.server_soft_version
 
 
     def __str__(self) -> str:
@@ -26,9 +26,36 @@ class Fridge:
         else:
             raise ValueError(f"Недопустимое название модели! Допустимо: {self.avalible_models}")
 
+
+    @classmethod
+    def update_server_soft_version(cls, new_soft_version: str):
+        cls.server_soft_version = new_soft_version
+
+    def update_soft(self):
+        if self.current_soft_version == self.server_soft_version:
+            print(f"Нет нужды обновлятся текущая версия соответствует серверной {self.current_soft_version}")
+
+        else:
+            print(f"Прошивка обновилась c {self.current_soft_version} на {self.server_soft_version}")
+            self.current_soft_version = self.server_soft_version
+
 # fridge_1 = Fridge("Владимир", "Биюрса Про Макс Ультра Пультра Эдишн!") # ValueError: Недопустимое название модели! Допустимо: ['Бирюса', 'Бирюса Плюс', 'Бирюса Про', 'Бирюса Про Макс']
 fridge_1 = Fridge("Владимир", "Бирюса Плюс")
 fridge_2 = Fridge("Батя", "Бирюса Про Макс")
 
 print(fridge_1)
 print(fridge_2)
+
+# fridge_1.update_server_soft_version("0.0.2")
+Fridge.update_server_soft_version("0.0.2")
+
+fridge_3 = Fridge("Брат", "Бирюса Про Макс")
+
+###############################
+print(fridge_1)
+print(fridge_2)
+print(fridge_3)
+
+fridge_1.update_soft()
+
+print(fridge_1)
